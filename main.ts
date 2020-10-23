7380f05ef660ce88afc5497813c75cc30946894a
rtator <66912559+rtator@users.noreply.github.com>
2020-10-23 07:28:54 -0700

    gib

--- main.ts
+++ main.ts
@@ -17,11 +17,11 @@ let mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
 let mySprite2 = sprites.create(img`
-    . . . . . f f f f f 
-    . . . . . f 1 1 1 f 
-    . . . . . f 1 1 1 f 
-    . . . . . f 1 1 1 f 
-    f f f f f f 1 1 1 f 
+    . . . . . . f f f f 
+    . . . . . . f 1 1 f 
+    . . . . . . f 1 1 f 
+    . . . . . . f 1 1 f 
+    f f f f f f f 1 1 f 
     f 1 1 1 1 1 1 1 1 f 
     f 1 1 1 1 1 1 1 1 f 
     f 1 1 1 1 1 1 1 1 f 
@@ -31,3 +31,4 @@ let mySprite2 = sprites.create(img`
     f f f f f f f f f f 
     `, SpriteKind.Player)
 scene.setBackgroundColor(9)
+controller.moveSprite(mySprite)
